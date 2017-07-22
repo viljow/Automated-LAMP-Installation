f44ad0bde93aa799646fceffd3697d7e460ca4aa
Viljo <viljo@m4d.xyz>
2017-07-22 14:48:17 +0100

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -6,25 +6,19 @@
 #
 #sudo ./SCRIPTNAME.sh
 
+root_mysql_passwd = ""
 
 echo "###################################################################################"
-echo "Please be Patient: Installation will start now.......and it will take some time :)"
+echo "Starting installation of LAMP"
 echo "###################################################################################"
 
-#Update the repositories
-
-sudo apt-get update
-
-#Apache, Php, MySQL and required packages installation
-
-sudo apt-get -y install apache2 php5 libapache2-mod-php5 php5-mcrypt php5-curl php5-mysql php5-gd php5-cli php5-dev mysql-client
+sudo apt update
+sudo apt -y install apache2 php5 libapache2-mod-php5 php5-mcrypt php5-curl php5-mysql php5-gd php5-cli php5-dev mysql-client
 php5enmod mcrypt
 
-#The following commands set the MySQL root password to MYPASSWORD123 when you install the mysql-server package.
-
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password MYPASSWORD123'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password MYPASSWORD123'
-sudo apt-get -y install mysql-server
+sudo debconf-set-selections <<< 'mariadb-server mariadb-server/root_password password $root_mysql_passwd'
+sudo debconf-set-selections <<< 'mariadb-server mariadb-server/root_password_again password $root_mysql_passwd'
+sudo apt-get -y install mariadb-server
 
 #Restart all the installed services to verify that everything is installed properly
 
@@ -35,9 +29,9 @@ service apache2 restart && service mysql restart > /dev/null
 echo -e "\n"
 
 if [ $? -ne 0 ]; then
-   echo "Please Check the Install Services, There is some $(tput bold)$(tput setaf 1)Problem$(tput sgr0)"
+   echo "An error has occured!"
 else
-   echo "Installed Services run $(tput bold)$(tput setaf 2)Sucessfully$(tput sgr0)"
+   echo "Install status: $(tput bold)$(tput setaf 2)SUCESS$(tput sgr0)"
 fi
 
 echo -e "\n"
