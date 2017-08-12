1f223dbebe44d87d37b602bcc5baee8b820b91f5
Viljo <viljo@m4d.xyz>
2017-08-12 12:46:34 +0100

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
-root_mysql_passwd = ""
+
+if [ -z $1 ]; then
+   echo "You must enter the root MySQL password as an argument!"
+   exit 1
+fi
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
 
 echo "###################################################################################"
-echo "Starting installation of LAMP"
+echo "Starting installation of LAMP stack"
 echo "###################################################################################"
 
-sudo apt update
-sudo apt -y install apache2 php5 libapache2-mod-php5 php5-mcrypt php5-curl php5-mysql php5-gd php5-cli php5-dev mysql-client
+apt update
+apt -y install apache2 php5 libapache2-mod-php5 php5-mcrypt php5-curl php5-mysql php5-gd php5-cli php5-dev mysql-client
 php5enmod mcrypt
 
-sudo debconf-set-selections <<< 'mariadb-server mariadb-server/root_password password $root_mysql_passwd'
-sudo debconf-set-selections <<< 'mariadb-server mariadb-server/root_password_again password $root_mysql_passwd'
-sudo apt-get -y install mariadb-server
+debconf-set-selections <<< 'mariadb-server-5.5 mysql-server/root_password password $1'
+debconf-set-selections <<< 'mariadb-server-5.5 mysql-server/root_password_again password $1'
+apt -y install mariadb-server
 
 echo -e "\n"
 
